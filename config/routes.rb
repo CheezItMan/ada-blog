f9aaef3479a9864c02465d66eb3ae8e95b66427b
CheezItMan <chris@adadevelopersacademy.org>
2017-10-21 14:44:24 -0700

    Added Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get '/auth/:github/callback', to: 'users#login', as: 'login'
+  delete '/users/logout', to: 'users#logout', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/blogs_controller_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+describe BlogsController do
+  it "should get index" do
+    get blogs_index_url
+    value(response).must_be :success?
+  end
+
+  it "should get show" do
+    get blogs_show_url
+    value(response).must_be :success?
+  end
+
+  it "should get new" do
+    get blogs_new_url
+    value(response).must_be :success?
+  end
+
+  it "should get create" do
+    get blogs_create_url
+    value(response).must_be :success?
+  end
+
+  it "should get edit" do
+    get blogs_edit_url
+    value(response).must_be :success?
+  end
+
+  it "should get update" do
+    get blogs_update_url
+    value(response).must_be :success?
+  end
+
+  it "should get destroy" do
+    get blogs_destroy_url
+    value(response).must_be :success?
+  end
+
+end
