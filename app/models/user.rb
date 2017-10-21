c7086935256f5bd96ffabdb268a26bdb63bb22f9
CheezItMan <chris@adadevelopersacademy.org>
2017-10-21 16:52:43 -0700

    Revert "Revert "did testing for login, and created logout method and testing""
    This reverts commit 81428daa32f9ea0ab7462e159c0e8c1b87a1a480.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
+  validates :uid, uniqueness: {scope: :provider}
 end
