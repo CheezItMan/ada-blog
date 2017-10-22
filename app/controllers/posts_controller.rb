22d93a0114d2d3155a35aa0a57b064a9cd812b06
CheezItMan <chris@adadevelopersacademy.org>
2017-10-21 17:35:57 -0700

    Added Posts index method & tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
-class BlogsController < ApplicationController
+class PostsController < ApplicationController
+  before_action :find_user
+
   def index
+    unless @user
+      flash[:error] = "That blog doesn't exist"
+      redirect_to root_path, status: 404
+    else
+      @posts = @user.posts
+    end
   end
 
   def show
@@ -19,4 +27,9 @@ class BlogsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def find_user
+      @user = User.find_by(id: params[:id])
+    end
 end
